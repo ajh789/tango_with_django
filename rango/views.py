274fd0430964d91830f2a1de60398923e3930f33
Andy Jiang <andy.h.jiang@gmail.com>
2017-12-30 21:58:38 +0800

    Chapter 7 : Forms Handling

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -23,3 +25,41 @@ def show_category(req, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(req, 'rango/category.html', context_dict)
+
+def add_category(req):
+    form = CategoryForm()
+    if req.method == 'POST':
+        print('See POST request')
+        form = CategoryForm(req.POST)
+        if form.is_valid():
+            print('Form is valid')
+            cat = form.save(commit=True)
+            print(cat, cat.slug)
+            return HttpResponseRedirect('/rango/')
+        else:
+            print('Form is not valid')
+            print('Error: %s' % form.errors)
+    print('Render page rango/add_category.html')
+    return render(req, 'rango/add_category.html', {'form': form});
+
+def add_page(req, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    form = PageForm()
+    if req.method == 'POST':
+        form = PageForm(req.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return HttpResponseRedirect(reverse('show_category', args=(category_name_slug,)))
+        else:
+            print(form.errors)
+    else:
+        pass
+    context_dict = {'form': form, 'category': category}
+    return render(req, 'rango/add_page.html', context_dict)
